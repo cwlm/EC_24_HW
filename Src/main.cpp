1b9e071be224e1b3e36be7400e88ff8f3593eb64
Cao Shuchen <csc123456zxcvbnm@gmail.com>
2023-10-17 21:20:20 +0800

    test:output

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,11 +2,14 @@
 // Created by yixin on 2023/10/2.
 //
 
+#include <iostream>
 #include "../Inc/main.h"
 #include "../Inc/robot.h"
 
 int main() {
     RobotPos pos{0,0,0,0};
     moveRobot(&pos, 1, 20000);
+    std::cout << pos.x << pos.y << std::endl;
+
     return 0;
 }
