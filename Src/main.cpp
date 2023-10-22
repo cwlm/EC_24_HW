9d6611650a4d1b51661ce12d23be361bc117582d
Cao Shuchen <csc123456zxcvbnm@gmail.com>
2023-10-22 16:24:15 +0800

    test: hit

--- Src/main.cpp
+++ Src/main.cpp
@@ -7,10 +7,13 @@
 #include "../Inc/robot.h"
 
 int main() {
-    struct Robot a[5];
+    // initialize
+    struct Robot a[5];// team a
+    struct Robot b[5];// team b
+
     for (int i = 0; i < 5; i++) {
         a[i].name[0] = 'a';
-        a[i].name[1] = '0' + char(i);
+        a[i].name[1] = '0' + i;
         a[i].pos.x = 0;
         a[i].pos.y = 0;
         a[i].pos.yaw = 0;
@@ -18,7 +21,21 @@ int main() {
         a[i].hp.current_hp = 100;
         a[i].hp.max_hp = 100;
     }
-    moveRobot(&a[0], 1, 2);
+    for (int i = 0; i < 5; i++) {
+        b[i].name[0] = 'b';
+        b[i].name[1] = '0' + i;
+        b[i].pos.x = 0;
+        b[i].pos.y = 0;
+        b[i].pos.yaw = 0;
+        b[i].pos.pitch = 0;
+        b[i].hp.current_hp = 100;
+        b[i].hp.max_hp = 100;
+    }
+
+    moveRobot(&a[0], 1, 5);
+    while (a[0].hp.current_hp > 0) {
+        hitRobot(&a[0], &b[0]);
+    }
 
     return 0;
 }
--- Src/robot.cpp
+++ Src/robot.cpp
@@ -1,22 +1,35 @@
 //
-// Created by yixin on 2023/10/2.
+// Created by csc on 2023/10/22.
 //
 
 #include "../Inc/robot.h"
 #include <cstdio>
 #include <cmath>
 
+// function to move robot
 Robot *moveRobot(Robot *robot, float dx, float dy) {
     printf("robot %s moved\nfrom x:%.2f y:%.2f\n", robot->name, robot->pos.x, robot->pos.y);
     robot->pos.x += dx;
     robot->pos.y += dy;
-    printf("to x:%.2f y:%.2f", robot->pos.x, robot->pos.y);
+    printf("to x:%.2f y:%.2f\n", robot->pos.x, robot->pos.y);
     return robot;
 }
 
+// function to attack another robot
 Robot *hitRobot(Robot *def, Robot *atk) {
-    float distance = sqrtf(powf(def->pos.x - atk->pos.x, 2) + powf(def->pos.y - atk->pos.y, 2));
+    printf("robot %s tried to hit robot %s.\n", atk->name, def->name);
 
-    def->hp.current_hp = def->hp.max_hp;
+    // check if succeeded
+    float distance = sqrtf(powf(def->pos.x - atk->pos.x, 2) + powf(def->pos.y - atk->pos.y, 2));
+    bool hitSucceed = distance <= (rand() % 1000 + 1) / 100.0;
+
+    // handle the result
+    if (hitSucceed) {
+        printf("Succeeded, hp of robot %s reduced from %d ", def->name, def->hp.current_hp);
+        def->hp.current_hp = def->hp.current_hp < 10 ? 0 : (def->hp.current_hp - 10);
+        printf("to %d.\n", def->hp.current_hp);
+    } else {
+        printf("Failed, hp of robot %s still %d.\n", def->name, def->hp.current_hp);
+    }
     return def;
 }
